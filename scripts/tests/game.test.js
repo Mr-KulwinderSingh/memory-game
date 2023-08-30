f582ff2c73b7b6ae3338c98a8e3a5568761979f6
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-08-30 05:58:55 +0000

    Add the three tests relavent to the last button key and turn In Pprogress key

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -37,6 +37,15 @@ describe("game object contains the correct keys", () => {
     test("turn number key exists", () => {
         expect("turnNumber" in game).toBe(true);
     });
+    test("lastButton key exists", () => {
+        expect("lastButton" in game).toBe(true);
+    });
+    test("turnInProgress key exists", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
+    test("turnInProgress key value is false", () => {
+        expect("turnInProgress" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {
